50b8e5c7a05d52bba3eb7dd1d31cbe328a393f50
Simone Sestito <simone@simonesestito.com>
2021-10-04 14:31:16 +0200

    Home: fix brightness setting (remove setState from build)

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:greenpass/models/data.dart';
 import 'package:greenpass/lang/localization.dart';
+import 'package:greenpass/models/data.dart';
 import 'package:greenpass/utils/globals.dart';
 import 'package:provider/provider.dart';
 import 'package:qr_flutter/qr_flutter.dart';
@@ -17,22 +17,35 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  var _maxBright = false;
+  double _originalBrightness = 0;
+  bool? _maxBrightClicked; // Click on the button
+
+  @override
+  void initState() {
+    Screen.brightness.then((brightness) {
+      _originalBrightness = brightness;
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
+    final passList = context.watch<GreenPassListData>().passes;
+    final _maxBright = _maxBrightClicked ?? passList.isNotEmpty;
+    if (_maxBright)
+      Screen.setBrightness(1);
+    else
+      Screen.setBrightness(_originalBrightness);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(Localization.of(context)!.translate('app_title')!),
         centerTitle: true,
         actions: [
           IconButton(
-            onPressed: () => !_maxBright
-                ? setState(() {
-                    _maxBright = true;
-                    Screen.setBrightness(1);
-                  })
-                : () {},
+            onPressed: () => setState(() {
+              _maxBrightClicked = !_maxBright;
+            }),
             icon: Icon(
               _maxBright
                   ? Icons.brightness_7_outlined
@@ -44,66 +57,9 @@ class _HomeScreenState extends State<HomeScreen> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 96),
-          child: Consumer<GreenPassListData>(builder: (context, data, _) {
-            if (data.passes.isEmpty) {
-              return Container(
-                alignment: Alignment.center,
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      'assets/images/no_qr_placeholder.png',
-                      height: 96,
-                      alignment: Alignment.center,
-                      color: Theme.of(context)
-                          .textTheme
-                          .bodyText2!
-                          .color!
-                          .withOpacity(0.4),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      Localization.of(context)!
-                          .translate('no_pass_placeholder')!,
-                    ),
-                  ],
-                ),
-              );
-            } else {
-              // Set max brightness if there's at least one pass
-              setState(() {
-                Screen.setBrightness(1);
-              });
-              return CarouselSlider.builder(
-                itemCount: data.passes.length,
-                itemBuilder: (context, i, _) => Card(
-                  elevation: 4,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text(
-                        data.passes[i].alias,
-                        style: Theme.of(context).textTheme.headline5,
-                      ),
-                      QrImage(data: data.passes[i].qrData),
-                    ],
-                  ),
-                ),
-                options: CarouselOptions(
-                  autoPlay: false,
-                  initialPage: data.passes.length - 1,
-                  reverse: true,
-                  viewportFraction: 0.8,
-                  aspectRatio: 9 / 12,
-                  enlargeCenterPage: true,
-                  enableInfiniteScroll: false,
-                ),
-              );
-            }
-          }),
+          child: passList.isEmpty
+              ? buildEmptyView(context)
+              : buildList(context, passList),
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -120,6 +76,59 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget buildEmptyView(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/images/no_qr_placeholder.png',
+            height: 96,
+            alignment: Alignment.center,
+            color:
+                Theme.of(context).textTheme.bodyText2!.color!.withOpacity(0.4),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Text(
+            Localization.of(context)!.translate('no_pass_placeholder')!,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildList(BuildContext context, List<GreenPass> passList) {
+    return CarouselSlider.builder(
+      itemCount: passList.length,
+      itemBuilder: (context, i, _) => Card(
+        elevation: 4,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              passList[i].alias,
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            QrImage(data: passList[i].qrData),
+          ],
+        ),
+      ),
+      options: CarouselOptions(
+        autoPlay: false,
+        initialPage: passList.length - 1,
+        reverse: true,
+        viewportFraction: 0.8,
+        aspectRatio: 9 / 12,
+        enlargeCenterPage: true,
+        enableInfiniteScroll: false,
       ),
     );
   }
