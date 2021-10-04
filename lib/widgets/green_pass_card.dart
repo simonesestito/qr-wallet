6238804e83c6946109bd00e90e001dec5e863567
Simone Sestito <simone@simonesestito.com>
2021-10-04 15:29:59 +0200

    Add popup menu to edit a single pass

--- lib/widgets/green_pass_card.dart
+++ lib/widgets/green_pass_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:greenpass/lang/localization.dart';
 import 'package:greenpass/models/data.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
@@ -14,14 +15,39 @@ class GreenPassCardView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       elevation: 4,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      child: Stack(
         children: [
-          Text(
-            pass.alias,
-            style: Theme.of(context).textTheme.headline5,
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                pass.alias,
+                style: Theme.of(context).textTheme.headline5,
+              ),
+              QrImage(data: pass.qrData),
+            ],
           ),
-          QrImage(data: pass.qrData),
+          Align(
+            alignment: Alignment.topRight,
+            child: PopupMenuButton(itemBuilder: (context) {
+              return [
+                PopupMenuItem(
+                    onTap: () {
+                      // TODO
+                    },
+                    child: Text(
+                      Localization.of(context)!.translate("pass_item_delete")!,
+                    )),
+                PopupMenuItem(
+                    onTap: () {
+                      // TODO
+                    },
+                    child: Text(
+                      Localization.of(context)!.translate("pass_item_rename")!,
+                    )),
+              ];
+            }),
+          ),
         ],
       ),
     );
