db3c84c5b03396c5d87bd227d37463993d699a0f
Simone Sestito <simone@simonesestito.com>
2021-10-05 09:35:26 +0200

    Fix typo

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -1,5 +1,5 @@
 class Globals {
-  // Some values (even different in type) to constantly reuse accross the app
+  // Some values (even different in type) to constantly reuse across the app
   static const double borderRadius = 16;
   static const double avatarRadius = 42;
   static const double borderWidth = 2;
