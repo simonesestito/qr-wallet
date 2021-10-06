d47152463de62866ac5fa9866ac1d0e313f1812c
Simone Sestito <simone@simonesestito.com>
2021-10-06 15:15:54 +0200

    Read app version from PackageInfo instead of hardcoded Globals

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:qrwallet/lang/localization.dart';
-import 'package:qrwallet/utils/globals.dart';
 import 'package:qrwallet/widgets/credits_card.dart';
 import 'package:qrwallet/widgets/title_headline.dart';
 
@@ -10,48 +10,54 @@ class SettingsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: CustomScrollView(slivers: [
-          SliverFillRemaining(
-            hasScrollBody: false,
-            child: Column(
-              children: [
-                TitleHeadline(
-                  title: Localization.of(context)!.translate('settings')!,
-                  backBtn: true,
-                  trailingBtn: Icons.article_outlined,
-                  trailingBtnAction: () => showLicensePage(
-                    context: context,
-                    applicationName:
-                        Localization.of(context)!.translate('app_title')!,
-                    applicationVersion: Globals.appVersion,
-                    applicationIcon: Container(
-                      padding: const EdgeInsets.all(16),
-                      child: Image.asset(
-                        'assets/images/logo.png',
-                        height: 128,
+    return FutureBuilder<PackageInfo>(
+      future: PackageInfo.fromPlatform(),
+      builder: (context, data) {
+        final appVersion = data.data?.version ?? '';
+        return Scaffold(
+          body: SafeArea(
+            child: CustomScrollView(slivers: [
+              SliverFillRemaining(
+                hasScrollBody: false,
+                child: Column(
+                  children: [
+                    TitleHeadline(
+                      title: Localization.of(context)!.translate('settings')!,
+                      backBtn: true,
+                      trailingBtn: Icons.article_outlined,
+                      trailingBtnAction: () => showLicensePage(
+                        context: context,
+                        applicationName:
+                            Localization.of(context)!.translate('app_title')!,
+                        applicationVersion: appVersion,
+                        applicationIcon: Container(
+                          padding: const EdgeInsets.all(16),
+                          child: Image.asset(
+                            'assets/images/logo.png',
+                            height: 128,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
+                    CreditsCard(
+                      description:
+                          Localization.of(context)!.translate('author_back')!,
+                      url: 'https://simonesestito.com/',
+                      image: Image.asset('assets/images/author_back.jpg'),
+                    ),
+                    CreditsCard(
+                      description:
+                          Localization.of(context)!.translate('author_front')!,
+                      url: 'https://www.minar.ml',
+                      image: Image.asset('assets/images/author_front.jpg'),
+                    ),
+                  ],
                 ),
-                CreditsCard(
-                  description:
-                      Localization.of(context)!.translate('author_back')!,
-                  url: 'https://simonesestito.com/',
-                  image: Image.asset('assets/images/author_back.jpg'),
-                ),
-                CreditsCard(
-                  description:
-                      Localization.of(context)!.translate('author_front')!,
-                  url: 'https://www.minar.ml',
-                  image: Image.asset('assets/images/author_front.jpg'),
-                ),
-              ],
-            ),
+              ),
+            ]),
           ),
-        ]),
-      ),
+        );
+      },
     );
   }
 }
--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -3,7 +3,6 @@ class Globals {
   static const double borderRadius = 16;
   static const double avatarRadius = 42;
   static const double borderWidth = 2;
-  static const String appVersion = '1.0.0';
   static const double buttonPadding = 24;
   static const String bannerAdsUnitId =
       'ca-app-pub-3883344461454437/5943921896';
