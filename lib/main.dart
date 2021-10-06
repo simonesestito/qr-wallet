db29ddfb5dcc597a908bcdf85215d906c0c579a8
Simone Sestito <simone@simonesestito.com>
2021-10-06 11:24:05 +0200

    Add AdMob Funding Choices consent (Not tested yet!)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:admob_consent/admob_consent.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -25,6 +26,8 @@ const APP_NAME = 'Green Pass Keeper';
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    AdmobConsent().show();
+
     return MaterialApp(
       title: APP_NAME,
       supportedLocales: [
@@ -39,8 +42,8 @@ class MyApp extends StatelessWidget {
       ],
       localeResolutionCallback: (locale, supportedLocales) {
         return supportedLocales.firstWhere(
-          (supportedLocale) =>
-              supportedLocale.languageCode == locale?.languageCode ||
+              (supportedLocale) =>
+          supportedLocale.languageCode == locale?.languageCode ||
               supportedLocale.countryCode == locale?.countryCode,
           orElse: () => supportedLocales.first,
         );
@@ -73,60 +76,60 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.white,
         scaffoldBackgroundColor: Colors.white,
         textSelectionTheme:
-            TextSelectionThemeData(cursorColor: Color(0xff1A753F)),
+        TextSelectionThemeData(cursorColor: Color(0xff1A753F)),
         textTheme: ThemeData.light().textTheme.copyWith(
-              bodyText1: TextStyle(
-                fontSize: 16,
-                color: Color(0xff505050),
-              ),
-              bodyText2: TextStyle(
-                fontSize: 16,
-                color: Color(0xff797979),
-              ),
-              subtitle1: TextStyle(
-                color: Color(0xff505050),
-                fontSize: 18,
-              ),
-              subtitle2: TextStyle(
-                color: Color(0xff505050),
-                fontWeight: FontWeight.w700,
-                fontSize: 16,
-              ),
-              headline1: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.w700,
-                color: Color(0xff505050),
-              ),
-              headline2: TextStyle(
-                fontSize: 36,
-                fontWeight: FontWeight.w700,
-                color: Color(0xff797979),
-              ),
-              headline3: TextStyle(
-                fontSize: 32,
-                fontWeight: FontWeight.w700,
-                color: Color(0xff505050),
-              ),
-              headline4: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.w700,
-                color: Color(0xff797979),
-              ),
-              headline5: TextStyle(
-                fontSize: 20,
-                color: Color(0xff505050),
-                fontWeight: FontWeight.w700,
-              ),
-              headline6: TextStyle(
-                fontSize: 18,
-                color: Color(0xff797979),
-              ),
-              button: TextStyle(
-                fontSize: 14,
-                color: Color(0xffffffff),
-                fontWeight: FontWeight.w700,
-              ),
-            ),
+          bodyText1: TextStyle(
+            fontSize: 16,
+            color: Color(0xff505050),
+          ),
+          bodyText2: TextStyle(
+            fontSize: 16,
+            color: Color(0xff797979),
+          ),
+          subtitle1: TextStyle(
+            color: Color(0xff505050),
+            fontSize: 18,
+          ),
+          subtitle2: TextStyle(
+            color: Color(0xff505050),
+            fontWeight: FontWeight.w700,
+            fontSize: 16,
+          ),
+          headline1: TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.w700,
+            color: Color(0xff505050),
+          ),
+          headline2: TextStyle(
+            fontSize: 36,
+            fontWeight: FontWeight.w700,
+            color: Color(0xff797979),
+          ),
+          headline3: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.w700,
+            color: Color(0xff505050),
+          ),
+          headline4: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.w700,
+            color: Color(0xff797979),
+          ),
+          headline5: TextStyle(
+            fontSize: 20,
+            color: Color(0xff505050),
+            fontWeight: FontWeight.w700,
+          ),
+          headline6: TextStyle(
+            fontSize: 18,
+            color: Color(0xff797979),
+          ),
+          button: TextStyle(
+            fontSize: 14,
+            color: Color(0xffffffff),
+            fontWeight: FontWeight.w700,
+          ),
+        ),
         snackBarTheme: SnackBarThemeData(
           backgroundColor: Color(0xFF323232),
         ),
@@ -157,60 +160,60 @@ class MyApp extends StatelessWidget {
         backgroundColor: Color(0xff313131),
         scaffoldBackgroundColor: Color(0xff313131),
         textSelectionTheme:
-            TextSelectionThemeData(cursorColor: Color(0xff1A753F)),
+        TextSelectionThemeData(cursorColor: Color(0xff1A753F)),
         textTheme: ThemeData.dark().textTheme.copyWith(
-              bodyText1: TextStyle(
-                fontSize: 16,
-                color: Color(0xffeeeeee),
-              ),
-              bodyText2: TextStyle(
-                fontSize: 16,
-                color: Color(0xffeeeeee),
-              ),
-              subtitle1: TextStyle(
-                color: Color(0xffeeeeee),
-                fontSize: 18,
-              ),
-              subtitle2: TextStyle(
-                color: Color(0xffeeeeee),
-                fontWeight: FontWeight.w700,
-                fontSize: 16,
-              ),
-              headline1: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.w700,
-                color: Color(0xffeeeeee),
-              ),
-              headline2: TextStyle(
-                fontSize: 36,
-                fontWeight: FontWeight.w700,
-                color: Color(0xffeeeeee),
-              ),
-              headline3: TextStyle(
-                fontSize: 32,
-                fontWeight: FontWeight.w700,
-                color: Color(0xffeeeeee),
-              ),
-              headline4: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.w700,
-                color: Color(0xffeeeeee),
-              ),
-              headline5: TextStyle(
-                fontSize: 20,
-                color: Color(0xffeeeeee),
-                fontWeight: FontWeight.w700,
-              ),
-              headline6: TextStyle(
-                fontSize: 18,
-                color: Color(0xffeeeeee),
-              ),
-              button: TextStyle(
-                fontSize: 14,
-                color: Color(0xffffffff),
-                fontWeight: FontWeight.w700,
-              ),
-            ),
+          bodyText1: TextStyle(
+            fontSize: 16,
+            color: Color(0xffeeeeee),
+          ),
+          bodyText2: TextStyle(
+            fontSize: 16,
+            color: Color(0xffeeeeee),
+          ),
+          subtitle1: TextStyle(
+            color: Color(0xffeeeeee),
+            fontSize: 18,
+          ),
+          subtitle2: TextStyle(
+            color: Color(0xffeeeeee),
+            fontWeight: FontWeight.w700,
+            fontSize: 16,
+          ),
+          headline1: TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.w700,
+            color: Color(0xffeeeeee),
+          ),
+          headline2: TextStyle(
+            fontSize: 36,
+            fontWeight: FontWeight.w700,
+            color: Color(0xffeeeeee),
+          ),
+          headline3: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.w700,
+            color: Color(0xffeeeeee),
+          ),
+          headline4: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.w700,
+            color: Color(0xffeeeeee),
+          ),
+          headline5: TextStyle(
+            fontSize: 20,
+            color: Color(0xffeeeeee),
+            fontWeight: FontWeight.w700,
+          ),
+          headline6: TextStyle(
+            fontSize: 18,
+            color: Color(0xffeeeeee),
+          ),
+          button: TextStyle(
+            fontSize: 14,
+            color: Color(0xffffffff),
+            fontWeight: FontWeight.w700,
+          ),
+        ),
       ),
       // Route of every possible screen
       routes: {
