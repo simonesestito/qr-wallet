146f2ec9b8bc512dd903b4990eb4e92c2bbb3c09
Simone Sestito <simone@simonesestito.com>
2021-10-08 12:19:15 +0200

    Block multiple parallel loadings

--- lib/widgets/interstitial_ad_loader.dart
+++ lib/widgets/interstitial_ad_loader.dart
@@ -4,16 +4,19 @@ import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class InterstitialAdLoader {
   static InterstitialAd? _loadedAd;
+  static bool _isLoading = false;
 
   static void loadAd() {
-    if (_loadedAd != null) return;
+    if (_loadedAd != null || _isLoading) return;
 
+    _isLoading = true;
     InterstitialAd.load(
         adUnitId: 'ca-app-pub-3940256099942544/1033173712',
         // FIXME: Create real unit
         request: AdRequest(),
         adLoadCallback: InterstitialAdLoadCallback(
           onAdLoaded: (InterstitialAd ad) {
+            _isLoading = false;
             _loadedAd = ad;
           },
           onAdFailedToLoad: (LoadAdError error) {
@@ -33,11 +36,8 @@ class InterstitialAdLoader {
     };
 
     _loadedAd?.fullScreenContentCallback = FullScreenContentCallback(
-      onAdDismissedFullScreenContent: (InterstitialAd ad) {
-        print('$ad onAdDismissedFullScreenContent.');
-        postAdCallback(ad);
-      },
-      onAdFailedToShowFullScreenContent: (InterstitialAd ad, AdError error) {
+      onAdDismissedFullScreenContent: postAdCallback,
+      onAdFailedToShowFullScreenContent: (ad, error) {
         print('$ad onAdFailedToShowFullScreenContent: $error');
         postAdCallback(ad);
       },
